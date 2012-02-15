11a249d964c87fc8c9770943b76e530cbbe669fb
Jeff <jeff.vera@gmail.com>
2012-02-15 11:34:02 -0500

    Initializing row with a valid line.

--- WeatherDataRow.cs
+++ WeatherDataRow.cs
@@ -7,6 +7,24 @@ namespace DataMungingKata
 {
 	public class WeatherDataRow
 	{
+		public WeatherDataRow() { }
+
+		public WeatherDataRow(string validLine)
+		{
+			InputLine = validLine;
+			string [] inputLineComponents =
+				InputLine.Split(" ".ToCharArray(),
+				StringSplitOptions.RemoveEmptyEntries);
+
+			DayNumber = 
+				int.Parse(inputLineComponents[0]);
+			HighTemperature = 
+				int.Parse(inputLineComponents[1]);
+			LowTemperature = 
+				int.Parse(inputLineComponents[2]);
+		}
+
+		public string InputLine { get; private set; }
 		public int DayNumber { get; set; }
 		public int LowTemperature { get; set; }
 		public int HighTemperature { get; set; }
--- WeatherDataRowTests.cs
+++ WeatherDataRowTests.cs
@@ -22,5 +22,19 @@ namespace DataMungingKata
 
 			Assert.That(spread, Is.EqualTo(10));
 		}
+
+		[Test]
+		public void CanParseValidWeatherLineTest()
+		{
+			string validLine = "   1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5";
+
+			WeatherDataRow row = 
+				new WeatherDataRow(validLine);
+
+			Assert.That(row, Is.Not.Null);
+			Assert.That(row.DayNumber, Is.EqualTo(1));
+			Assert.That(row.LowTemperature, Is.EqualTo(59));
+			Assert.That(row.HighTemperature, Is.EqualTo(88));
+		}
 	}
 }
